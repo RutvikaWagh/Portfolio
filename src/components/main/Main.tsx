6d7f4f767826a6ad34f84a13ac4d157e805e4a94
Rutvika Wagh <shrutiwagh31@gmail.com>
2023-10-01 14:12:05 +0530

    new branch added

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -8,22 +8,21 @@ import React from 'react';
 const Main: React.FC = () => {
 	return (
 		<div className='max-width-900px margin-auto'>
-			MainMainMainMainMainMain MainMainMainMain MainMainMainMainMainMain MainMainMainMain MainMainMainMainMainMain
-			MainMainMainMain MainMainMainMainMainMain MainMainMainMain MainMainMainMainMainMain
-			MainMainMainMainMainMainMainMainMainMain MainMainMainMainMainMainMainMainMainMain
-			MainMainMainMainMainMainMainMainMainMain MainMainMainMain MainMainMainMainMainMain
-			MainMainMainMainMainMainMainMainMainMain MainMainMainMainMainMainMainMainMainMain
-			MainMainMainMainMainMainMainMainMainMain MainMainMainMain MainMainMainMainMainMain
-			MainMainMainMainMainMainMainMainMainMain MainMainMainMainMainMainMainMainMainMain
-			MainMainMainMainMainMainMainMainMainMain MainMainMainMain MainMainMainMainMainMain
-			MainMainMainMainMainMainMainMainMainMain MainMainMainMainMainMainMainMainMainMain
-			MainMainMainMainMainMainMainMainMainMain MainMainMainMainMainMainMainMainMainMain MainMainMainMain
-			MainMainMainMainMainMain MainMainMainMainMainMainMainMainMainMain MainMainMainMainMainMainMainMainMainMain
-			MainMainMainMainMainMainMainMainMainMain MainMainMainMain MainMainMainMainMainMain
-			MainMainMainMainMainMainMainMainMainMain MainMainMainMainMainMainMainMainMainMain
-			MainMainMainMainMainMainMainMainMainMain MainMainMainMainMainMainMainMainMainMain MainMainMainMain
-			MainMainMainMainMainMain MainMainMainMainMainMainMainMainMainMain MainMainMainMainMainMainMainMainMainMain
-			MainMainMainMainMainMainMainMainMainMain MainMainMainMain
+			<div className='main'>
+				<h1>Hi, This is Rutvika Wagh</h1>
+				<h2>
+					A Second year under graduate at Cummins College of Engineering for Women, Pune Studying Computer
+					Engineering
+				</h2>
+				<p>I am based in Pune,Maharashtra</p>
+				<p>My Native place in Dhule Maharashtra</p>
+				<p>My aspirations are to Work towards creating software that makes life easier and more meaningful.</p>
+				<div>
+					<a className='btn' href=''>
+						view Resume
+					</a>
+				</div>
+			</div>
 		</div>
 	);
 };
