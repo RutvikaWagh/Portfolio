1e34539a09adc1ca08fc29dec988182f4b49ba9a
Rutvika Wagh <shrutiwagh31@gmail.com>
2023-10-01 14:26:11 +0530

    Removed css

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -10,15 +10,12 @@ const Main: React.FC = () => {
 		<div className='max-width-900px margin-auto'>
 			<div className='main'>
 				<h1>Hi, This is Rutvika Wagh</h1>
-				<h2>
+				<p>
 					A Second year under graduate at Cummins College of Engineering for Women, Pune Studying Computer
 					Engineering
-				</h2>
-				<p>I am based in Pune,Maharashtra</p>
-				<p>My Native place in Dhule Maharashtra</p>
-				<p>My aspirations are to Work towards creating software that makes life easier and more meaningful.</p>
+				</p>
 				<div>
-					<a className='btn' href=''>
+					<a className='resume-button' href=''>
 						view Resume
 					</a>
 				</div>
