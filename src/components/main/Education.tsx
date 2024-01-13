df727a51e705c04aee3ab234ca181569a62d55a8
Rutvika Wagh <rutvikawagh@gmail.com>
2024-01-13 09:23:11 +0530

    css added

--- src/components/main/Education.tsx
+++ src/components/main/Education.tsx
@@ -8,31 +8,33 @@ const Education: React.FC = () => {
           <h2>Education</h2>
         </div>
         <div className="education">
-        <table>
-        <tr><th>Degree</th>
-          <th>Year of Passing</th>
-          <th>Institute Name</th>
-          <th>Performance</th>
-          </tr>
-          <tr>
-          <td>SSC</td>
-          <td>2020</td>
-          <td>S.T.T.K. Mahajan HighSchool</td>
-            <td>95.60 %</td>
-          </tr>
-          <tr>
-          <td>HSC</td>
-          <td>2022</td>
-          <td>G.D. Mali Secondary School</td>
-          <td>83.83 %</td>
-          </tr>
-          <tr>
-          <td>B-Tech (appear)</td>
-          <td>2026</td>
-          <td>MKSSS Cummins College of Engineering for women Pune</td>
-          <td>FY - 8.3 %ile</td>
-          </tr>
-         </table>
+     
+      <div className="course">
+      <div className="title-c">Course Name</div>
+      <ul>
+        <li>SSC</li>
+        <li>HSC</li>
+        <li>B-Tech in Computer Engineering</li>
+      </ul>
+      </div>
+     
+      <div className="years">
+      <div className="title-c">Year of Passing</div>
+      <ul>
+        <li>2020</li>
+        <li>2022</li>
+        <li>2026</li>
+      </ul>
+      </div>
+     
+      <div className="institute">
+      <div className="title-c">Institution</div>
+      <ul>
+        <li>S.T.T.K. Mahajan HighSchool,Deopur Dhule</li>
+        <li>G.D. Mali Higher Secondary College,Dhule</li>
+        <li>MKSSS Cummins College of Engineering for women,Pune</li>
+      </ul>
+      </div>
       </div>
       </div>
     </section>
