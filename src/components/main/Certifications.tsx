dfb2febff897f9ab2670a8f808c70c85dbd7bde5
Rutvika <rutvikawagh@gmail.com>
2024-03-03 16:06:32 +0530

    updated table

--- src/components/main/Certifications.tsx
+++ src/components/main/Certifications.tsx
@@ -6,79 +6,79 @@ const Certificate: React.FC = () => {
       <div className="title">
         <h2>Certificates</h2>
       </div>
-       <div className="certificates">
-      <div className="temp">
-        <table>
-          <tr>
-            <th>Sr No.</th>
-            <th>Year of Achievement</th>
-            <th>Issued By</th>
-            <th>Certifications</th>
-          </tr>
-          <tr>
-            <td>1.</td>
-            <td>2022</td>
-            <td>E-Club,SJMSOS IIT-BOMBAY</td>
-            <td>Certificate of Appriciation</td>
-          </tr>
-          <tr>
-            <td>2.</td>
-            <td>2023</td>
-            <td>Sololearn</td>
-            <td>Introduction to Javascript</td>
-          </tr>
-          <tr>
-            <td>3.</td>
-            <td>2023</td>
-            <td>Sololearn</td>
-            <td>Introduction to HTML</td>
-          </tr>
-          <tr>
-            <td>4.</td>
-            <td>2023</td>
-            <td>Sololearn</td>
-            <td>Introduction to CSS</td>
-          </tr>
-          <tr>
-            <td>5.</td>
-            <td>2023</td>
-            <td>Sololearn</td>
-            <td>Introduction to Python</td>
-          </tr>
-          <tr>
-            <td>6.</td>
-            <td>2023</td>
-            <td>Sololearn</td>
-            <td>Introduction to Java</td>
-          </tr>
-          <tr>
-            <td>7.</td>
-            <td>2023</td>
-            <td>Dewtown</td>
-            <td>Certificate of Appriciation</td>
-          </tr>
-          <tr>
-            <td>8.</td>
-            <td>2023</td>
-            <td>Mozilla Campus Club</td>
-            <td>Certificate of Appriciation</td>
-          </tr>
-          <tr>
-            <td>9.</td>
-            <td>2023</td>
-            <td>Developer Student Clubs</td>
-            <td>Certificate of Completion</td>
-          </tr>
-          <tr>
-            <td>10.</td>
-            <td>2023</td>
-            <td>Dewtown</td>
-            <td>Certificate of Participation</td>
-          </tr>
-        </table>
-       
-      
-      </div>
+      <div className="certificates">
+        <div className="temp">
+          <table>
+            <tbody>
+              <tr>
+                <th>Sr No.</th>
+                <th>Year of Achievement</th>
+                <th>Issued By</th>
+                <th>Certifications</th>
+              </tr>
+              <tr>
+                <td>1.</td>
+                <td>2022</td>
+                <td>E-Club,SJMSOS IIT-BOMBAY</td>
+                <td>Certificate of Appriciation</td>
+              </tr>
+              <tr>
+                <td>2.</td>
+                <td>2023</td>
+                <td>Sololearn</td>
+                <td>Introduction to Javascript</td>
+              </tr>
+              <tr>
+                <td>3.</td>
+                <td>2023</td>
+                <td>Sololearn</td>
+                <td>Introduction to HTML</td>
+              </tr>
+              <tr>
+                <td>4.</td>
+                <td>2023</td>
+                <td>Sololearn</td>
+                <td>Introduction to CSS</td>
+              </tr>
+              <tr>
+                <td>5.</td>
+                <td>2023</td>
+                <td>Sololearn</td>
+                <td>Introduction to Python</td>
+              </tr>
+              <tr>
+                <td>6.</td>
+                <td>2023</td>
+                <td>Sololearn</td>
+                <td>Introduction to Java</td>
+              </tr>
+              <tr>
+                <td>7.</td>
+                <td>2023</td>
+                <td>Dewtown</td>
+                <td>Certificate of Appriciation</td>
+              </tr>
+              <tr>
+                <td>8.</td>
+                <td>2023</td>
+                <td>Mozilla Campus Club</td>
+                <td>Certificate of Appriciation</td>
+              </tr>
+              <tr>
+                <td>9.</td>
+                <td>2023</td>
+                <td>Developer Student Clubs</td>
+                <td>Certificate of Completion</td>
+              </tr>
+              <tr>
+                <td>10.</td>
+                <td>2023</td>
+                <td>Dewtown</td>
+                <td>Certificate of Participation</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </section>
   );
